bfa647af0166979fd01e2a6af9505c787dc414c6
Michael-August <michaelfrancis135@gmail.com>
2023-05-07 15:07:45 +0100

    ADDED: Built a DB model for the data

---
+++ src/app/DB/products.ts
@@ -0,0 +1,106 @@
+export const products = [
+    {
+        id: 1,
+        img: 'assets/images/Product A.png',
+        name: 'Sweat Top',
+        sizes: ['XS', 'S', 'M', 'L'],
+        price: 50.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product A.png',
+            'assets/images/Product A.png',
+            'assets/images/Product A.png',
+        ]
+    },
+
+    {
+        id: 2,
+        img: 'assets/images/Product B.png',
+        name: 'Roundneck Blowse',
+        sizes: ['XS', 'S', 'M'],
+        price: 20.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product B.png',
+            'assets/images/Product B.png',
+            'assets/images/Product B.png',
+        ]
+    },
+
+    {
+        id: 3,
+        img: 'assets/images/Product C.png',
+        name: 'Roundneck Blowse',
+        sizes: ['XS', 'S', 'M'],
+        price: 25.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product C.png',
+            'assets/images/Product C.png',
+            'assets/images/Product C.png',
+        ]
+    },
+
+    {
+        id: 4,
+        img: 'assets/images/Product D.png',
+        name: 'Roundneck Blowse',
+        sizes: ['XS', 'S', 'M'],
+        price: 50.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product D.png',
+            'assets/images/Product D.png',
+            'assets/images/Product D.png',
+        ]
+    },
+
+    {
+        id: 5,
+        img: 'assets/images/Product A.png',
+        name: 'Sweat Top',
+        sizes: ['XS', 'S', 'M', 'L'],
+        price: 50.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product A.png',
+            'assets/images/Product A.png',
+            'assets/images/Product A.png',
+        ]
+    },
+
+    {
+        id: 6,
+        img: 'assets/images/Product C.png',
+        name: 'Roundneck Blowse',
+        sizes: ['XS', 'S', 'M'],
+        price: 25.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product C.png',
+            'assets/images/Product C.png',
+            'assets/images/Product C.png',
+        ]
+    },
+
+    {
+        id: 7,
+        img: 'assets/images/Product B.png',
+        name: 'Roundneck Blowse',
+        sizes: ['XS', 'S', 'M'],
+        price: 20.00,
+        colors: ['gray', 'black', 'green'],
+        description: "Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands.",
+        thumbnails: [
+            'assets/images/Product B.png',
+            'assets/images/Product B.png',
+            'assets/images/Product B.png',
+        ]
+    },
+]
